bf7a35e991132b8edc8f43789f069bd819b504e7
davidvonpku@gmail.com <davidvonpku@gmail.com>
2013-07-14 09:37:58 +0200

    swr2 recorder

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:swr2-recorder.py
+	./swr2-recorder.py test-swr2.mp3 60
---
+++ swr2-recorder.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# This is some code for the SWR2 recorder
+# 2013-07-14: Accio started
+
+import urllib2
+import re
+import time
+import argparse
+
+record_len=120;
+outfile="/home/david/Music/test-swr2.mp3"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("outfile",
+                    help="Output file")
+parser.add_argument("interval", 
+                    help="Time interval of recording, in seconds",
+                    type=int)
+args = parser.parse_args();
+
+conn=urllib2.urlopen("http://mp3-live.swr.de/swr2_m.m3u")
+r1=conn.read()
+stream_url=r1.splitlines()[2]
+target=open(args.outfile, "w")
+conn=urllib2.urlopen(stream_url)
+start_time=time.time();
+end_time=start_time+args.interval;
+
+while time.time()<end_time:
+    target.write(conn.read(1024))
+
+target.close()
+    
+
+    
